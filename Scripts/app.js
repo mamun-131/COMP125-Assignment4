692771e095f23564ecf96e7198f7362de1c39ce1
300872772 <mamun_131@hotmail.com>
2016-08-12 18:07:48 -0400

    added new components

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,6 @@
 /**
- * This is a mini profile extended web application which is all about Md Mamunur Rahman 
- * 
+ * This is a mini profile extended web application which is all about Md Mamunur Rahman
+ *
  * @FileName: app.js
  * @Author Md Mamunur Rahman
  * @student ID: 300872772
@@ -8,76 +8,64 @@
  * @website: http://mamun-miniportfolio-extended.azurewebsites.net
  * @description: this file is main javascript file for the website
  */
-
-
 // IIFE - Immediately Invoked Function Expression
 (function () {
-
     // define objects, arrays and variabes
     var paragraphHeading = [];
     var paragraphElements = [];
     var xhrParagraphDataFile;
-
+    var contactForm;
+    var firstName;
+    var lastName;
+    var email;
+    var contactNumber;
+    var message;
     //FILE READING PROCESS HANDLING FOR PARAGRAPH GENERATION BEGIN+++++++++++++++++++
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     /**
      * This function is for reading data from file
-     * 
+     *
      * @method readData
-     * @return {void} 
+     * @return {void}
      */
     function readData() {
-
-            // data loaded                                      everything is ok
+        // data loaded                                      everything is ok
         if ((xhrParagraphDataFile.readyState === 4) && (xhrParagraphDataFile.status === 200)) {
-
             var paragraphFile = JSON.parse(xhrParagraphDataFile.responseText);
             var paragraphs = paragraphFile.paragraphs;
-
-            var counter=0;
+            var counter = 0;
             //extracting data from object
-            paragraphs.forEach(function (paragraph) {                
-	            if (paragraphElements[counter]) {
-                    	paragraphHeading[counter].innerHTML = paragraph.paragraphHeading;
-                        paragraphElements[counter].innerHTML = paragraph.paragraphDetail;
-                }   
+            paragraphs.forEach(function (paragraph) {
+                if (paragraphElements[counter]) {
+                    paragraphHeading[counter].innerHTML = paragraph.paragraphHeading;
+                    paragraphElements[counter].innerHTML = paragraph.paragraphDetail;
+                }
                 console.log(paragraph.paragraphDetail);
-            counter++;
-
+                counter++;
             }, this);
-
-        } 
-
+        }
     }
     /**
      * This function is for connecting with file and to ope for file reading
-     * 
+     *
      * @method readParagraphFile
-     * @return {void} 
+     * @return {void}
      */
     function readParagraphFile() {
         xhrParagraphDataFile = new XMLHttpRequest(); // create xhr object
-
         xhrParagraphDataFile.open("GET", "../Scripts/paragraphs.json", true); // open request
-
         xhrParagraphDataFile.send(null); // send request
-
         xhrParagraphDataFile.addEventListener("readystatechange", readData);
-
-
     }
-
     //FILE READING PROCESS HANDLING FOR PARAGRAPH GENERATION END+++++++++++++++++++
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
     //FORM HANDLING IN CONTACT PAGE BEGIN++++++++++++++++++++++++++++++++++++++++++++
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     /**
      * This function is for handling form in contact page
-     * 
+     *
      * @method formHandlingContactPage
-     * @return {void} 
+     * @return {void}
      */
     function formHandlingContactPage() {
         //creat a reference to the button in contact page
@@ -87,18 +75,14 @@
             //event listener 
             sendButton.addEventListener("click", sendButtonClick);
         }
-
-
         //create a references for fields in contatc page form
-        var firstName = document.getElementById("firstName");
-        var lastName = document.getElementById("lastName");
-        var email = document.getElementById("email");
-        var contactNumber = document.getElementById("contactNumber");
-        var message = document.getElementById("message");
-
+        firstName = document.getElementById("firstName");
+        lastName = document.getElementById("lastName");
+        email = document.getElementById("email");
+        contactNumber = document.getElementById("contactNumber");
+        message = document.getElementById("message");
         //create a reference to the form
-        var contactForm = document.getElementById("contactForm");
-
+        contactForm = document.getElementById("contactForm");
         if (contactForm) {
             //event listener
             contactForm.addEventListener("submit", contactFormSubmit);
@@ -106,20 +90,18 @@
     }
     /**
      * This function is a event handler function for sendButtonClick event
-     * 
+     *
      * @method sendButtonClick
-     * @return {void} 
+     * @return {void}
      */
     function sendButtonClick(event) {
         console.log("clicked");
-
     }
-
     /**
      * This function is a event handler function for formsubmit event
-     * 
+     *
      * @method contactFormSubmit
-     * @return {void} 
+     * @return {void}
      */
     function contactFormSubmit(event) {
         event.preventDefault();
@@ -129,9 +111,9 @@
     }
     /**
      * This function shows the input from each form field on console
-     * 
+     *
      * @method showFormInput
-     * @return {void} 
+     * @return {void}
      */
     function showFormInput() {
         console.log("+++++++++++++++++++++++++++++++++++");
@@ -144,15 +126,13 @@
     }
     //FORM HANDLING IN CONTACT PAGE END++++++++++++++++++++++++++++++++++++++++++++
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
     /**
      * This function binds paragraph element and headings
-     * 
+     *
      * @method handlingParagraph
-     * @return {void} 
+     * @return {void}
      */
     function handlingParagraph() {
-
         //bonding ids with paragraphHeading arrays
         paragraphHeading[0] = document.getElementById("homeHeading");
         paragraphHeading[1] = document.getElementById("projectsHeading");
@@ -160,7 +140,6 @@
         paragraphHeading[3] = document.getElementById("blogsHeading");
         paragraphHeading[4] = document.getElementById("photographyHeading");
         paragraphHeading[5] = document.getElementById("contactHeading");
-
         //bonding ids with paragraphElements arrays
         paragraphElements[0] = document.getElementById("homePara");
         paragraphElements[1] = document.getElementById("projectsPara");
@@ -169,14 +148,24 @@
         paragraphElements[4] = document.getElementById("photographyPara");
         paragraphElements[5] = document.getElementById("contactPara");
     }
-
     // app entry function
     function init() {
         formHandlingContactPage();
         handlingParagraph();
         readParagraphFile();
+        document.getElementById("mamun").innerHTML = greeter.greet();
     }
-
     // call init funciton when window finishes loading
     window.addEventListener("load", init);
 })();
+var Greeter = (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        return "<h1>" + this.greeting + "</h1>";
+    };
+    return Greeter;
+}());
+;
+var greeter = new Greeter("Hello, world!");
